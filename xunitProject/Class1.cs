bbfcb177f80d6dbe418c2caca763405d2989900a
fenggaoyao <Fenggaoyao@163.com>
2018-07-16 12:54:47 +0800

    提交github

---
+++ xunitProject/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using xunitdemo;
+using Xunit;
+using Moq;
+
+namespace xunitProject
+{
+    public class Class1
+    {
+        [Fact]
+        public void AdditionTest()
+        {
+            var calc = new Calculator();
+            var result = calc.Addition(1, 2);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void AdditionTest2()
+        {
+            var calc = new Calculator();
+            var result = calc.Addition(1, 2);
+            Assert.Equal(4, result);
+        }
+        [Fact]
+        public void CreateUser()
+        {
+            var mockobject = new Mock<IUserRepository>();
+            var user = new User { Age = 10, Name = "hhh" };
+            mockobject.Setup(a => a.Add(user)).Returns(true);
+
+            var usermanage = new UserManager(mockobject.Object);
+            Assert.Throws<FormatException>(()=>usermanage.CreateNewUser(user));
+        }
+
+
+    }
+}
---
+++ xunitdemo/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xunitdemo
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public interface IUserRepository
+    {
+        bool Add(User user);
+    }
+
+    public class UserManager
+    {
+        private IUserRepository userRepository;
+        public UserManager(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public bool CreateNewUser(User user)
+        {
+            if (user.Age < 0 || string.IsNullOrEmpty(user.Name))
+            {
+                throw new FormatException("hello error");
+            }
+            else
+            {
+                return this.userRepository.Add(user);
+            }
+        }
+    }
+
+    
+}
